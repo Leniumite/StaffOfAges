8e00a9240a29be63b97795ef10fd8f7c6799c5a5
Leniumite <48475428+Leniumite@users.noreply.github.com>
2022-01-28 22:48:16 +0100

    Added aim and trigger for the aim

--- Assets/Script/LaserAim.cs
+++ Assets/Script/LaserAim.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class LaserAim : MonoBehaviour
 {
+    [Header("Aim")]
+    public Transform center; //center of baby object
     [SerializeField] private GameObject laser;
+    private Vector3 aimDirection;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,19 +19,26 @@ public class LaserAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.LookAt(point);
-        
-        // Young
-        if(Input.GetMouseButtonDown(0))
-        {
-            laser.SetActive(true);
-        }
+        //Aim at
+        AimInput();
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        // Old
-        if (Input.GetMouseButtonDown(1))
+        // Young
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             laser.SetActive(true);
+            //TODO method
         }
+        else
+            laser.SetActive(false);
+    }
+
+    public void AimInput()
+    {
+        // Vector between mouse and center of the player, normalized
+        Vector2 tempDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - center.position;
+        aimDirection = new Vector3(tempDir.x, tempDir.y, 0);
+        aimDirection.Normalize();
     }
 }
