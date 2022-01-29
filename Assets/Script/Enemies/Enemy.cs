9c89025454ecb88e7b42428479e38a7e94c0d374
Leniumite <48475428+Leniumite@users.noreply.github.com>
2022-01-29 11:46:44 +0100

    Added behaviour to ray

--- Assets/Script/Enemies/Enemy.cs
+++ Assets/Script/Enemies/Enemy.cs
@@ -11,23 +11,41 @@ public class Enemy : MonoBehaviour
     public Sprite sprite;
     public bool isShieldWhite;
     public bool isShieldViolet;
+    public float resToRay; //Basically seconds to get effect from the ray
+    private float tempRes;
+
+    [Header("Attack")]
+    public bool CaC;
+    public float cooldown;
+    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
     {
         //gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+        tempRes = resToRay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(lifeStage > 4 || lifeStage < 1)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void GetYoung()
     {
         if (isShieldWhite)
             return;
+
+        tempRes -= Time.deltaTime;
+        if(tempRes <= 0)
+        {
+            lifeStage--;
+            tempRes = resToRay;
+        }
 
         Debug.Log("young");
     }
@@ -36,6 +54,13 @@ public class Enemy : MonoBehaviour
     {
         if (isShieldViolet)
             return;
+
+        tempRes -= Time.deltaTime;
+        if (tempRes <= 0)
+        {
+            lifeStage++;
+            tempRes = resToRay;
+        }
 
         Debug.Log("Old");
     }
