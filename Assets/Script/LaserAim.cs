9a8c4f37dcceac662c1dcd447390ff9a08b02a2b
Leniumite <48475428+Leniumite@users.noreply.github.com>
2022-01-29 13:07:35 +0100

    Better Ray please don't touch

--- Assets/Script/LaserAim.cs
+++ Assets/Script/LaserAim.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class LaserAim : MonoBehaviour
 {
     [Header("Aim")]
-    public Transform center; //center of baby object
+    public Transform Body; 
     public float longueurRayon;
     [SerializeField] private GameObject laser;
     private Vector3 aimDirection;
     private Vector3 tempPos;
+    private CircleCollider2D col;
 
     [Header("Laser")]
     public Color lifeColor;
@@ -20,45 +21,43 @@ public class LaserAim : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        col = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Vector between mouse and center of the player, normalized
-        Vector3 aimDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - center.position;
-        aimDir = aimDir.normalized;
+        // Vector between mouse and body of the player, normalized
+        Vector3 aimDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Body.position;
+        Vector3 tempDir = aimDir;
+        tempDir.z = 0;
 
-        Vector2 transform2D = new Vector2(transform.position.x + (transform.localScale.x / 2), transform.position.y);
+        Vector2 transform2D = new Vector2(Body.position.x + (col.radius), Body.position.y);
 
-        RaycastHit2D hit = Physics2D.Raycast(center.position, aimDir, longueurRayon + center.localScale.x);
+        RaycastHit2D hit = Physics2D.Raycast(Body.position, aimDir, longueurRayon + col.radius, ~LayerMask.GetMask("Default"));
         if (hit.collider != null)
         {
-            if (hit.collider.gameObject.tag == "enemy")
-            {
-                //Debug.Log("test");
-                enemy = hit.collider.gameObject;
-                Vector3 distToEnemy = hit.point - transform2D;
+            Debug.Log("test");
+            enemy = hit.collider.gameObject;
+            Vector3 distToEnemy = hit.point - transform2D;
 
-                laser.transform.localScale = new Vector3(distToEnemy.magnitude, 0.5f, 0);
-                laser.transform.position = (transform2D + hit.point) / 2;
-            }
+            laser.transform.localScale = new Vector3(distToEnemy.magnitude, 0.5f, 0);
+            laser.transform.position = Body.position + (laser.transform.localScale.x /2 + col.radius) * tempDir.normalized;
         }
         else
         {
             enemy = null;
-            Vector3 tempDir = aimDir;
-            tempDir.z = 0;
-            tempPos = tempDir.normalized * longueurRayon + center.position;
-            laser.transform.position = tempDir.normalized * (longueurRayon + center.localScale.x / 2) + center.position;
-            laser.transform.localScale = new Vector3((tempPos - center.position).magnitude * 2, 0.5f, 0);
+
+            tempPos = tempDir.normalized * longueurRayon + Body.position;
+            laser.transform.position = tempDir.normalized * ((longueurRayon/2) + col.radius) + Body.position;
+            laser.transform.localScale = new Vector3((tempPos - Body.position).magnitude, 0.5f, 0);
         }
 
 
         //Aim at
         AimInput(aimDir);
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-        center.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        laser.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
